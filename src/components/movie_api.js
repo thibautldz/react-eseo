1a3d5a3df092dd95994c4b4e03c2038194cf024c
Antoine LAURENT <antoine.laurent@reseau.eseo.fr>
2022-01-21 22:55:46 +0100

    HTTPS

--- src/components/movie_api.js
+++ src/components/movie_api.js
@@ -1,6 +1,6 @@
 export const apiKey = "28e63299ff61593c038bcf59b3414211";
-export const base_uri = "http://api.themoviedb.org/3/";
-export const images_uri = "http://image.tmdb.org/t/p/w500/";
+export const base_uri = "https://api.themoviedb.org/3/";
+export const images_uri = "https://image.tmdb.org/t/p/w500/";
 
 const generic_fetch = async (base, param_url, output) => {
     return fetch(base+param_url+"?api_key="+apiKey)
